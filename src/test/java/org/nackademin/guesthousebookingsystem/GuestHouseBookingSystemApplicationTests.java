a7a93f01e38af14ab16b19bc8a7afdcb22e81aac
Karwan Ali <karwana0000@gmail.com>
2026-05-11 17:43:15 +0200

    Initial commit

---
+++ src/test/java/org/nackademin/guesthousebookingsystem/GuestHouseBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nackademin.guesthousebookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestHouseBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
