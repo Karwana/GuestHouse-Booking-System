dc96de2334f0f0c700307e4bbe4d617169f05a7b
Karwan Ali <karwana0000@gmail.com>
2026-05-17 19:00:57 +0200

    Add edit and delete function for customers

--- src/main/java/org/nackademin/guesthousebookingsystem/controller/CustomerController.java
+++ src/main/java/org/nackademin/guesthousebookingsystem/controller/CustomerController.java
@@ -2,7 +2,6 @@ package org.nackademin.guesthousebookingsystem.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.nackademin.guesthousebookingsystem.dto.CustomerDto;
-import org.nackademin.guesthousebookingsystem.entity.Customer;
 import org.nackademin.guesthousebookingsystem.service.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -36,6 +35,25 @@ public class CustomerController {
             @ModelAttribute CustomerDto customerDto) {
 
         customerService.saveCustomer(customerDto);
+        return "redirect:/customers";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editCustomer(@PathVariable Long id, Model model) {
+        model.addAttribute("customer", customerService.getCustomerById(id));
+        return "customers/edit";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateCustomer(@PathVariable Long id,
+                                 @ModelAttribute CustomerDto customerDto) {
+        customerService.updateCustomer(id, customerDto);
+        return "redirect:/customers";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
         return "redirect:/customers";
     }
 
