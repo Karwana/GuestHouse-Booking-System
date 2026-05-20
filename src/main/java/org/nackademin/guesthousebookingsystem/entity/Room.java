1bd70cd2140f61688613d1f3da5f5831b9b84c99
Fahim K <fahim.khan@yh.nackademin.se>
2026-05-20 19:30:26 +0200

    add validation annotations to Room entity

--- src/main/java/org/nackademin/guesthousebookingsystem/entity/Room.java
+++ src/main/java/org/nackademin/guesthousebookingsystem/entity/Room.java
@@ -1,6 +1,8 @@
 package org.nackademin.guesthousebookingsystem.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,10 +17,14 @@ public class Room {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private int roomNumber;
+    @NotNull
+    @Min(1)
+    private Integer roomNumber;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     private RoomType roomType;
 
+    @Min(0)
     private int extraBeds;
 }
